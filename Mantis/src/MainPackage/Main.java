570a830f90763b38a6c2c1dc33173d1d57c23ebc
karthick <47217897+karthick-R18@users.noreply.github.com>
2020-01-03 18:23:13 +0530

    Add files via upload
    Updation

--- Mantis/src/MainPackage/Main.java
+++ Mantis/src/MainPackage/Main.java
@@ -3,6 +3,8 @@ package MainPackage;
 import java.util.List;
 import java.io.BufferedWriter;
 import java.io.File;  
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.PrintWriter;  
 import java.util.*; 
@@ -17,7 +19,8 @@ class Main {
 	String URL = "";
 	WebElement Element;
 	String Pwd = "Gotyeah44";
-	int indexCount = 3;
+	int indexCount = 3; // Projects start from 3 in xpath
+	PrintWriter writer;
 
 	Main() {
 		System.setProperty(
@@ -26,14 +29,14 @@ class Main {
 		driver = new ChromeDriver();
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
 		Main mantis = new Main();
 		mantis.logOnToMantis();
 
 	}
 
-	public void logOnToMantis() {
+	public void logOnToMantis() throws FileNotFoundException {
 
 		driver.get("http://192.168.168.205/wrgmantis/login_page.php");
 		setXpath("//*[@id='username']");
@@ -45,7 +48,14 @@ class Main {
 		Element.submit();
 
 		// select projects
+		writer = new PrintWriter(new FileOutputStream(
+				new File("C:\\Users\\rkarthick\\Desktop\\Dailymantisdata.txt"), true /* append = true */));
+		
+		writer.write("\r\n\r\n============== "+new java.util.Date()+" ==============");
 		clickProjects();
+		writer.write("\r\n\r\n===========================================================");
+		writer.close();
+
 
 	}
 
@@ -54,36 +64,31 @@ class Main {
 			List<WebElement> Elements = driver
 					.findElements(By
 							.xpath("/html/body/div[2]/div[2]/div[2]/div[1]/div/div[4]/table/tbody/tr"));
-			
-			//int count = Elements.size();
+						
 			int i=0;
-			BufferedWriter writer = new BufferedWriter(
-                    new FileWriter("C:/Users/rkarthick/Desktop/Dailymantisdata.txt", true)  //Set true for append mode
-                );  
-				writer.newLine();   //Add new line
-				
+			
 			for (WebElement ele : Elements) {
-				writer.write("============================");
+				
 				if (ele.getAttribute("class").equals("") && (ele.getText()).contains("rkarthick")) {
 					for(int j=1;j>0;j++){
 						if(Elements.get(i-j).getAttribute("class").equals("row-category-history")){
 							System.out.println(Elements.get(i-j).getText());
-							writer.write(Elements.get(i-j).getText());
+							writer.write("\r\n\r\n"+Elements.get(i-j).getText());
 							break;
 						}					
 							
 						}
 					System.out.println(ele.getText());
-					writer.write(ele.getText());
+					writer.write("\r\n"+ele.getText());
 					}
 				i++;
 						
 					}			
 			clickProjects();
-			writer.close();
-
+			
 		} catch (Exception e) {
-
+			writer.write("\r\n\r\n===========================================================");
+			writer.close();
 			 // System.out.println("Exception occurred : "+e);
 		}
 
